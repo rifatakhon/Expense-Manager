1e7e00d8d20d118c8de8c25f710068b959da90a3
Md Rifat Hossain <rifatakhon@gmail.com>
2024-06-05 17:41:06 +0600

    dashbaord template added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,32 +5,41 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title', 'Dashboard')</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('assets/libs/morris.js/morris.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/icons.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/js/config.js') }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
+    <body>
+        <div class="layout-wrapper">
+            @include('partials.navigation')
+            <!-- Main Content -->
             <main>
-                {{ $slot }}
+                @yield('content')
             </main>
+
+
         </div>
+
+        <!-- App js -->
+    <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+
+    <!-- Knob charts js -->
+    <script src="{{ asset('assets/libs/jquery-knob/jquery.knob.min.js') }}"></script>
+
+    <!-- Sparkline Js-->
+    <script src="{{ asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
+
+    <script src="{{ asset('assets/libs/morris.js/morris.min.js') }}"></script>
+
+    <script src="{{ asset('assets/libs/raphael/raphael.min.js') }}"></script>
+
+    <!-- Dashboard init-->
+    <script src="{{ asset('assets/js/pages/dashboard.js') }}"></script>
     </body>
 </html>
